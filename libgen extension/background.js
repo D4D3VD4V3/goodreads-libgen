12570444144d6147d57bcbb8d5ac769288a69e61
Dave <dev.davidsundararaj@gmail.com>
2019-05-04 22:21:42 +0530

    need to filter in content.js

--- libgen extension/background.js
+++ libgen extension/background.js
@@ -1,52 +1,65 @@
-function onlyUnique(value, index, self) { 
+function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-		
-		$.ajax({url: request, success: function (data, status, xhr) {
-			var match = /(\d+) books found/.exec(data)[1];
-			console.log(match);
-			
-			if (match == 0)
-				sendResponse("");
-			else
-			{
-				var dllink = "http://booksdl.org/get.php?"
-				//var match = $(data).filter("tr");
-				var datastring = JSON.stringify(data);
-				//console.log(datastring);
-				var md5s = datastring.match(/md5=[a-f0-9]{32}/gm).filter(onlyUnique);
-				//var exts = Array.from(datastring.matchAll(/<td\snowrap>(epub|mobi|azw|azw3)<\/td>/gm));
-				var exts = [];
-				var rx = new RegExp(/<td\snowrap>(epub|mobi|azw|azw3|)<\/td>/gm);
-				while((match = rx.exec(datastring)) != null) {
-						exts.push(match[1]);
-				}
-				console.log(md5s);
-				console.log(exts);
-				sendResponse([[md5s[0], exts[0]]]);
-			}
-	}});
-		var req = new XMLHttpRequest(); 
-		req.open("GET", request);
-		req.onload = function() { 
-		if (req.readyState === 4 && req.status === 200) {
-		
-			var match = /(\d+) books found/.exec(req.responseText)[1];
-			console.log(match);
-			
-			if (match == 0)
-				sendResponse("");
-			else
-			{
-				var match = /(\d+) books found/.exec(req.responseText)[1];
-				sendResponse(true);
-			}
-		}
-		};
-		req.send(null);
+		console.log(request);
+        $.ajax({
+            url: request,
+            success: function(data, status, xhr) {
+                var match = /(\d+) books found/.exec(data)[1];
+                console.log(match);
+
+                if (match == 0)
+                    sendResponse("unavailable");
+                else {
+                    var dllink = "http://booksdl.org/get.php?"
+                    //var match = $(data).filter("tr");
+                    var datastring = JSON.stringify(data);
+                    //console.log(datastring);
+                    var md5s = datastring.match(/md5=[a-f0-9]{32}/gm).filter(onlyUnique);
+                    //var exts = Array.from(datastring.matchAll(/<td\snowrap>(epub|mobi|azw|azw3)<\/td>/gm));
+                    var exts = [];
+                    //var rx = new RegExp(/<td\snowrap>(epub|mobi|azw|azw3)<\/td>/gm);
+					//filtering extensions here discards the order of hashes resulting in mismatch of hash with selected ext. 2 sols: regex every ext on libgen or proper scraping.
+                    var rx = new RegExp(/<td\snowrap>(epub|mobi)<\/td>/gm);
+                    while ((match = rx.exec(datastring)) != null) {
+                        exts.push(match[1]);
+                    }
+                    console.log(md5s);
+                    console.log(exts);
+                    mobiIndex = exts.indexOf("mobi");
+                    if (mobiIndex != -1) {
+                        console.log([
+                            [md5s[mobiIndex], "mobi"]
+                        ]);
+                        sendResponse([
+                            [md5s[mobiIndex], "mobi"]
+                        ]);
+                    }
+					else
+                        sendResponse("customunavailable");
+                }
+            }
+        });
+        var req = new XMLHttpRequest();
+        req.open("GET", request);
+        req.onload = function() {
+            if (req.readyState === 4 && req.status === 200) {
+
+                var match = /(\d+) books found/.exec(req.responseText)[1];
+                console.log(match);
+
+                if (match == 0)
+                    sendResponse("");
+                else {
+                    var match = /(\d+) books found/.exec(req.responseText)[1];
+                    sendResponse(true);
+                }
+            }
+        };
+        req.send(null);
         return true;
     }
 );
--- libgen extension/content.js
+++ libgen extension/content.js
@@ -6,11 +6,17 @@ chrome.runtime.sendMessage(
     searchstring,
     function (response) {
 		console.log(response);
-		if (response!="")
+		if (response!="unavailable")
 		{
 			$(`<button style="color: white; padding: 10px;border-radius: 4px;text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);background: rgb(28, 184, 65);"><a href=${searchstring} style="color: white;" target="_blank">Get it on libgen.io</a></button>`).insertAfter(".wtrButtonContainer");
+			
+			if(response!="customunavailable")
+			{
 			dlurl = dlstring.concat(response[0][0]);
-			$(`<button style="color: white; padding: 10px;border-radius: 4px;text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);background: rgb(28, 184, 65);"><a href=${dlurl} style="color: white;" target="_blank">$(response[1][0])</a></button>`).insertAfter(".wtrButtonContainer");
+			btntext = response[1][0];
+			alert(btntext);
+			$(`<button style="color: white; padding: 10px;border-radius: 4px;text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);background: rgb(28, 184, 65);"><a href=${dlurl} style="color: white;" target="_blank">$(btntext)</a></button>`).insertAfter(".wtrButtonContainer");
+			}
 	}
 		else
 			$(`<button style="color: white; padding: 10px;border-radius: 4px;text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);background: rgb(184, 28, 65);"><a href=${searchstring} style="color: white;" target="_blank">Not available on libgen.io</a></button>`).insertAfter(".wtrButtonContainer");
